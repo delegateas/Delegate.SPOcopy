c7137498db2cc1653d16d3df8739fadc737d7559
Ramón Soto Mathiesen <rsm@delegate.dk>
2016-02-03 13:36:38 +0100

    Center image

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -70,7 +70,7 @@ Delegate.SPOcopy.Error.txt:
 > created in about two hours on the SharePoint Online instance, with no errors
 > whatsoever *)
 
-(** <img src="img/files_local_vs_sp.png" width="50%" height="50%" /> *)
+(** <img src="img/files_local_vs_sp.png" width="50%" height="50%" align="middle" /> *)
 
 (**
 How it works and limitations
