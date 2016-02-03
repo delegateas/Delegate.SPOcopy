fef30bd235a72f25a8424dca70c10b1b2ab51011
Ramón Soto Mathiesen <rsm@delegate.dk>
2016-02-03 13:25:14 +0100

    Updated GH-Pages

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -70,7 +70,6 @@ Delegate.SPOcopy.Error.txt:
 > created in about two hours on the SharePoint Online instance, with no errors
 > whatsoever *)
 
-
 (** <img src="img/files_local_vs_sp.png" /> *)
 
 (**
